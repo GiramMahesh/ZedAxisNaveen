68a40e1fc74aaf19a022cbff849a540345b62ba1
Mahesh <maheshgiram55@gmail.com>
2020-07-06 22:18:45 +0530

    First Commit Uploading all the files

---
+++ ZedAxisOnline/src/test/java/testcases/LoginPageTest.java
@@ -0,0 +1,44 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import base.TestBase;
+import pages.HomePage;
+import pages.LoginPage;
+
+
+
+public class LoginPageTest extends TestBase{
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest(){
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp(){
+		initialization();
+		loginPage = new LoginPage();	
+	}
+
+	@Test(priority=3)
+	public void loginTest(){
+//		 loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+
+}
